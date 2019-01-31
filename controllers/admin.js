09dd091315795f017b8e6f9d837a644c06af5b94
Raybeedrill <demezis1994@gmal.com>
2019-01-31 02:40:09 +0200

    made edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,10 +2,14 @@ const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res) => {
-    res.render('admin/add-product', {pageTitle: 'Add Product', path:'/admin/add-product'});
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        edit: false
+    });
 }
 
-exports.postAddProduct =  (req, res) => {
+exports.postAddProduct = (req, res) => {
     const title = req.body.title;
     const imgUrl = req.body.imgUrl;
     const price = req.body.price;
@@ -15,8 +19,32 @@ exports.postAddProduct =  (req, res) => {
     res.redirect('/')
 }
 
+exports.getEditProduct = (req, res) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, (product) => {
+        if (!product) {
+            return redirect('/');
+        }
+    
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            edit: editMode,
+            product: product
+        });
+    });
+}
+
 exports.getProducts = (req, res) => {
     Product.fetchAll((products) => {
-        res.render('admin/products', {prods: products, pageTitle: 'Admin product list', path: '/admin/products'});
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin product list',
+            path: '/admin/products'
+        });
     });
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const adminContorller = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/add-product', adminContorller.getAddProduct);
+router.get('/edit-product/:productId', adminContorller.getEditProduct);
 router.get('/products', adminContorller.getProducts);
 
 router.post('/add-product', adminContorller.postAddProduct);
